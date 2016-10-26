fa4b8f4f3589464264dc40ca8299f4797c35badf
Matt McInnis <mcinnism@ca.ibm.com>
2016-10-26 10:55:15 -0500

    Added crazy matt bot for realz now

--- players/challengerBot.js
+++ players/challengerBot.js
@@ -1,32 +1,14 @@
-function chance(amount) {
-  return Math.random() > amount;
-}
-
 module.exports = function () {
 
   var info = {
-    name: "CrazyRoboMatt"
+    name: "Nameless Challenger",
+    email: "",
+    btcWallet: ""
   };
 
   function update(game) {
-    var wager;
     if (game.state !== "complete") {
-      var wager = game.betting.call
-      switch(game.state) {
-      case 'pre-flop':
-        if (chance(0.7)) wager = game.betting.raise
-        break;
-      case 'flop':
-        if (chance(0.7)) wager = game.betting.raise
-        break;
-      case 'turn':
-        if (chance(0.7)) wager = game.betting.raise
-        break;
-      case 'river':
-        if (chance(0.7)) wager = game.betting.raise
-        break;
-      }
-      return wager;
+      return game.betting.call
     }
   }
 
