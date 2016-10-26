5bf1b1efeedcb23aa9923a7260da470e8e16fefc
Matt McInnis <mcinnism@ca.ibm.com>
2016-10-26 10:46:11 -0500

    Added CrazyMattBot!

--- players/challengerBot.js
+++ players/challengerBot.js
@@ -1,14 +1,32 @@
+function chance(amount) {
+  return Math.random() > amount;
+}
+
 module.exports = function () {
 
   var info = {
-    name: "Nameless Challenger",
-    email: "",
-    btcWallet: ""
+    name: "CrazyRoboMatt"
   };
 
   function update(game) {
+    var wager;
     if (game.state !== "complete") {
-      return game.betting.call
+      var wager = game.betting.call
+      switch(game.state) {
+      case 'pre-flop':
+        if (chance(0.7)) wager = game.betting.raise
+        break;
+      case 'flop':
+        if (chance(0.7)) wager = game.betting.raise
+        break;
+      case 'turn':
+        if (chance(0.7)) wager = game.betting.raise
+        break;
+      case 'river':
+        if (chance(0.7)) wager = game.betting.raise
+        break;
+      }
+      return wager;
     }
   }
 
